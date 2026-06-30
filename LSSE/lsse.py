6eb3b68360a9464079457e6f9c8d7c93fd854dc1
Heretic <adamboulaaz92@gmail.com>
2026-06-30 06:34:46 +0100

    Add files via upload

--- LSSE/lsse.py
+++ LSSE/lsse.py
@@ -6,7 +6,7 @@ yellow = "\033[33m"
 
 class LSSE:
     def __init__(self):
-        self.scripts_list = ["http-title","http-cert","http-robots","dns-subdomain-fuzzing","spider","script","http-dir"]
+        self.scripts_list = ["http-title","http-cookie","http-methods","http-headers","http-cert","http-robots","dns-subdomain-fuzzing","spider","script","http-dir","dns-lookup","dns-zone-transfer","dns-ns"]
 
     def script_list(self,sname,ports=None,redirect=None,domain=None,dns=None,wordlist=None,url=None,max_pages=None,max_depth=None,extensions=None,status_codes=None):
         if sname in self.scripts_list:
@@ -14,6 +14,15 @@ class LSSE:
                 case "http-title":
                     from LSSE.scripts.safe.analysis.http_https.http_title import threaded_http_title
                     threaded_http_title(domain,ports,redirect)
+                case "http-headers":
+                    from LSSE.scripts.safe.analysis.http_https.http_headers import run
+                    run(domain, port=int(ports[0]), redirect=redirect)
+                case "http-cookie":
+                    from LSSE.scripts.safe.analysis.http_https.http_cookie import run
+                    run(domain, port=int(ports[0]), redirect=redirect)
+                case "http-methods":
+                    from LSSE.scripts.safe.discovery.http_https.http_methods import run
+                    run(domain, port=int(ports[0]))
                 case "http-cert":
                     from LSSE.scripts.safe.analysis.https.http_cert import threaded_tls_ssl_cert_info
                     threaded_tls_ssl_cert_info(domain,ports)
@@ -51,6 +60,24 @@ class LSSE:
                     except Exception as e:
                         print(f"\n{red}[!] {e}{reset}")
                         exit(1)
+                case "dns-lookup":
+                    from LSSE.scripts.safe.discovery.dns.dns_lookup import dns_lookup
+                    if dns == None:
+                        dns_lookup(domain)
+                    else:
+                        dns_lookup(domain,dns_server=dns)
+                case "dns-zone-transfer":
+                    from LSSE.scripts.medium.extracting.dns.dns_zone_transfer import runzon
+                    if dns == None:
+                        runzon(domain)
+                    else:
+                        runzon(domain, dns_server=dns)
+                case "dns-ns":
+                    from LSSE.scripts.safe.discovery.dns.dns_ns import run
+                    if dns == None:
+                        run(domain)
+                    else:
+                        run(domain, dns_server=dns)
                 case _:
                     print(f"\n{yellow}[!] Script not found {reset}\n")
                     sys.exit(2)
